637c386f0c6870df6534d69056c85e5e70f2e775
Grassellini Thomas <thomas.grassellini@telecomnancy.eu>
2018-11-19 14:34:52 +0100

    Création éxécutable python 3.X et débuggage de celui-ci

--- README.md
+++ README.md
@@ -17,3 +17,25 @@ sudo python3 -m pip install -U scikit-learn
 sudo python3 -m pip install pyqtgraph
 
 sudo python3 -m pip install pyopengl
+
+
+Pour windows :
+
+Télécharger le fichier pyqt4 avec la version correspondante à votre python: https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyqt4
+
+lancer dans un powershell la commande suivante : 
+
+C:\path\where\wheel\is\> pip install PyQt4-4.11.4-cp36-cp36m-win_amd64.whl  ( cette exemple est fait avec une version de pyqt4 pour python 3.6)
+
+Ensuite réaliser les commandes suivante : ( certaines peuvent ne rien faire si la librairie est déjà à jour) 
+
+pip install nibabel
+
+pip install numpy
+
+pip install scikit-learn
+
+pip install pyqtgraph
+
+pip install pyopengl
+
--- setup.py
+++ setup.py
@@ -10,39 +10,45 @@ from os import path
 # console application).
 
 path = sys.path
-#path = sys.path + [r"C:\Users\thoma\Desktop\LOG\brainMapper"]
 #print(path)
 
 # Dependencies are automatically detected, but it might need fine tuning.
 #'./UI/mainView.py'
 #("C:\Users\thoma\Desktop\LOG\brainMapper/UI/resources.py","C:\Users\thoma\Desktop\LOG\brainMapper\build\exe.win-amd64-2.7\lib\resources.py")
-includefiles = ["UI.bat","mkl_intel_thread.dll",'./ressources']
-includes = ["os","sys","time","csv","nibabel","numpy","scipy","sklearn","PyQt4.QtCore", "PyQt4.QtGui",
-"pyqtgraph.opengl", "sip", "pyqtgraph.graphicsItems", "atexit","pyqtgraph.debug","numpy.core._methods",
-"numpy.lib.format", "scipy.sparse.csgraph._validation","sklearn.tree._criterion","scipy.ndimage._ni_support",
-"scipy.ndimage._ni_docstrings","pyqtgraph.ThreadsafeTimer","OpenGL.platform.win32","pyqtgraph.opengl.shaders",
-"pyqtgraph.opengl.glInfo","OpenGL.platform.glx","OpenGL.platform.darwin"]
-excludes = []
+includefiles = ["UI.bat",'./ressources']
+includes = ["os","sys","time","csv","nibabel","numpy","scipy","sklearn","sip","atexit","OpenGL","PyQt4","pyqtgraph",
+"PyQt4.QtCore", "PyQt4.QtGui",
+"pyqtgraph.opengl", "pyqtgraph.graphicsItems","pyqtgraph.debug",
+"numpy.core._methods", "numpy.lib.format",
+"scipy.sparse.csgraph._validation","scipy.ndimage._ni_support","scipy.ndimage._ni_docstrings",
+"sklearn.tree._criterion",
+"pyqtgraph.ThreadsafeTimer","pyqtgraph.opengl.shaders",
+"pyqtgraph.opengl.glInfo","pyqtgraph.units","pyqtgraph.reload",
+"pyqtgraph.PlotData","pyqtgraph.ordereddict","pyqtgraph.frozenSupport","pyqtgraph.configfile",
+"OpenGL.arrays._buffers","OpenGL.arrays._strings","OpenGL.arrays.buffers",
+"OpenGL.arrays.ctypesarrays","OpenGL.arrays.ctypesparameters","OpenGL.arrays.ctypespointers","OpenGL.arrays.lists",
+"OpenGL.arrays.nones","OpenGL.arrays.numbers","OpenGL.arrays.numpybuffers","OpenGL.arrays.numpymodule",
+"OpenGL.arrays.strings"]
+#conflit de nom entre scipy.spatial.ckdtree dans les lib et dans le build
+
+
+excludes = ["scipy.spatial.cKDTree"]
 packages = ["UI_builder","clustering_components","ourLib"]
 #zip_includes= ["BrainMapper.pyc","./UI/mainView.pyc","./UI/resources.pyc"]
-zip_includes =["BrainMapper.pyc"]
-#"./UI/clusteringView.pyc","./UI/editCollectionsView.pyc","./UI/exportView.pyc","./UI/calculationView.pyc",
+zip_includes =[]
 #d = dirname(dirname(abspath(__file__)))
 #sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
-#zip_include_packages= ["../UI","clustering_components"]
-#zip_include_packages= ["clustering_components","./UI/UI_builder"]
 zip_include_packages=[]
 zip_exclude_packages=[]
 
 
-
 if sys.platform == "win32":
-    pass
+  includes+= ["OpenGL.platform.win32"]
+  includefiles+=["mkl_intel_thread.dll"]
 elif sys.platform == "linux2":
-    pass
+  includes+= ["OpenGL.platform.glx"]
 else:
-    pass
+  includes+= ["OpenGL.platform.darwin"]
 
 # pour que les bibliotheques binaires de /usr/lib soient recopiees aussi sous Linux
 binpathincludes = []
@@ -53,7 +59,7 @@ if sys.platform == "linux2":
 optimize = 0
 
 # si True, n'affiche que les warning et les erreurs pendant le traitement cx_freeze
-silent = False
+silent = True
 
 # construction du dictionnaire des options
 options = {"path": path,
@@ -83,17 +89,11 @@ base=None
 icone = None
 if sys.platform == "win32":
     icone = "icone.ico"
-    #icone = "brain.png"
 
 
 cible_1 = Executable(
     script="UI.py",
     base=base,
-    #compress=False,  # <= ne pas generer de fichier zip
-    #copyDependentFiles=True,
-    #appendScriptToExe=True,
-    #appendScriptToLibrary=False,  # <= ne pas generer de fichier zip
-    #icon=icone
     )
 
 setup(  name = "BrainMapper",
@@ -101,16 +101,3 @@ setup(  name = "BrainMapper",
         description = "Executable version of BrainMapper",
         options = {'build_exe': options}, 
         executables = [cible_1])
-
-#Executable("./UI/UI.py", base=base)
-
-# def find_data_file(filename):
-#     if getattr(sys, 'frozen', False):
-#         # The application is frozen
-#         datadir = os.path.dirname(sys.executable)
-#     else:
-#         # The application is not frozen
-#         # Change this bit to match where you store your data files:
-#         datadir = os.path.dirname(__file__)
-
-#     return os.path.join(datadir, filename)
